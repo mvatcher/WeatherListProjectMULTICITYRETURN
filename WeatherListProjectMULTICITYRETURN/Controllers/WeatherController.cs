54e47fd12b98737eaa5cd44162204380834592a4
mvatcher <165951598+mvatcher@users.noreply.github.com>
2024-05-03 09:59:19 -0700

    Add project files.

---
+++ WeatherListProjectMULTICITYRETURN/Controllers/WeatherController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace WeatherProject.Controllers
+{
+    [Route("api/[controller]")]
+    public class WeatherController : Controller
+    {
+
+        [HttpGet("[action]")] // /{city}
+        public async Task<IActionResult> City(List<string> myCities)
+        {
+            using (var client = new HttpClient())
+
+            {
+                try
+                {
+                    var stringResult = "";
+                    List<string> listReturns = new List<string>();
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var options = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                    };
+
+
+
+                    foreach (string city in myCities)
+                    {
+                        Console.WriteLine(city);
+                        var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=ab2ac8f50aa62fc7ddfdce05ec5b1fac&units=imperial");
+                        response.EnsureSuccessStatusCode();
+                        stringResult = await response.Content.ReadAsStringAsync();
+                        Console.WriteLine(stringResult);
+                        listReturns.Add(stringResult);
+
+                    }
+
+                    Dictionary<string, float> cityWeather = new Dictionary<string, float>();
+                    foreach (string city in listReturns)
+                    {
+                        var rawWeather = JsonSerializer.Deserialize<OpenWeatherResponse>(city, options);
+                        cityWeather.Add(rawWeather.Name, rawWeather.Main.Temp);
+                    }
+
+                    //var rawWeatherA = JsonSerializer.Deserialize<OpenWeatherResponse>(listReturns[0], options);
+                    //var rawWeatherB = JsonSerializer.Deserialize<OpenWeatherResponse>(listReturns[1], options);
+                    //var rawWeatherC = JsonSerializer.Deserialize<OpenWeatherResponse>(listReturns[2], options);
+
+
+
+                    return Ok(//new
+                              //{
+                              //City = rawWeatherA.Name,
+                              //Temp = rawWeatherA.Main.Temp,
+                              //Summary = string.Join(",", rawWeatherA.Weather.Select(x => x.main)),
+                              //Wind = rawWeatherA.Wind.Speed,
+                              //CityB = rawWeatherB.Name,
+                              //TempB = rawWeatherB.Main.Temp,
+                              //SummaryB = string.Join(",", rawWeatherB.Weather.Select(x => x.main)),
+                              //WindB = rawWeatherB.Wind.Speed,
+                              //CityC = rawWeatherC.Name,
+                              //TempC = rawWeatherC.Main.Temp,
+                              //SummaryC = string.Join(",", rawWeatherC.Weather.Select(x => x.main)),                        
+                              //WindC = rawWeatherC.Wind.Speed
+                              //}
+                    cityWeather
+                        );
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
+        }
+
+    }
+
+}
+
